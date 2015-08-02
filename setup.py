b59d0743f82c5e49134afdb4b4f9aa07218a747b
Tiffany8 <tiffany@TRWilliams.local>
2015-08-02 14:45:08 -0700

    Update version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('README.md') as readme_file:
     readme = readme_file.read()
 setup(name='trw_text_string',
-      version='0.0.4',
+      version='0.0.5',
       author='T Williams',
       description='Package for a challenge.',
       long_description=readme,
--- trw_text_string/WordPairGenerator.py
+++ trw_text_string/WordPairGenerator.py
@@ -5,7 +5,7 @@
 
 import re
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 
 class WordPairGenerator(object):
